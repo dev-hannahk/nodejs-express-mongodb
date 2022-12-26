7209dc6985be6454d5d7cd0e6924b090b220df34
dev-hannahk <heejinkim@Heejinui-MacBookPro.local>
2022-12-27 00:55:59 +0900

    chapter 5. templates

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,9 +1,42 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  console.log(req.params);
-  res.send("Watch");
+const fakeUser = {
+  username: "Hannah",
+  loggedIn: false,
 };
-export const edit = (req, res) => res.send("Edit");
+
+export const trending = (req, res) => {
+  // const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // const videos = [];
+  const videos = [
+    {
+      id: 1,
+      title: "Video #1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+    },
+    {
+      id: 2,
+      title: "Video #2",
+      rating: 3,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+    },
+    {
+      id: 3,
+      title: "Video #3",
+      rating: 4,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
+
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
 export const deleteVideo = (req, res) => res.send("Delete Video");
 export const upload = (req, res) => res.send("Upload Video");
--- src/server.js
+++ src/server.js
@@ -10,8 +10,11 @@ const app = express();
 
 const logger = morgan("dev"); // morgan inform us path, method, etc.
 
+//express 가 views라는 폴더를 바라봄
+app.set("view engine", "pug");
+// src 디렉토리 안의 views 폴더를 바라볼 수 있도록 셋팅
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", viedoRouter);
