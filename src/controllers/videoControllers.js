8f1346a49d7a33a023e69655e024d1042fcb1c45
dev-hannahk <dev.hannahk@gmail.com>
2023-01-01 23:36:14 +0900

    chapter 6. mongodb and mongoose - part 3

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -9,25 +9,58 @@ export const home = async (req, res) => {
   }
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
 
-  return res.render("watch", { pageTitle: `Watch` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  } else {
+    return res.render("watch", { pageTitle: video.title, video });
+  }
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
 
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  } else {
+    return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  }
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
   // req.body에는 form을 통해 submit된 data의 key-value값을 가짐
   // default = undefined
   // express.json() or express.urlencoded() 파싱을 통해 값을 받아올 수 있음
-  const { title } = req.body;
-  return res.redirect(`/videos/${id}`);
+  const { title, description, hashtags } = req.body;
+  // const video = await Video.findById(id);
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  } else {
+    await Video.findByIdAndUpdate(id, {
+      title,
+      description,
+      hashtags: hashtags
+        .split(",")
+        .map((hashtag) =>
+          hashtag.startsWith("#") ? ` ${hashtag}` : `#${hashtag}`
+        ),
+    });
+    // video.title = title;
+    // video.description = description;
+    // video.hashtags = hashtags
+    //   .split(",")
+    //   .map((hashtag) =>
+    //     hashtag.startsWith("#") ? ` ${hashtag}` : `#${hashtag}`
+    //   );
+    // await video.save();
+    return res.redirect(`/videos/${id}`);
+  }
 };
 
 export const getUpload = (req, res) => {
@@ -41,7 +74,11 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((hashtag) => `#${hashtag}`),
+      hashtags: hashtags
+        .split(",")
+        .map((hashtag) =>
+          hashtag.startsWith("#") ? ` ${hashtag}` : `#${hashtag}`
+        ),
     });
   } catch (error) {
     return res.render("upload", {
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -9,14 +9,17 @@ import {
 
 const viedoRouter = express.Router();
 
+// 숫자만 들어오는 정규식을 제거하면 upload 경로를 id로 착각하기 떄문에 가장 맨 위에 위치해야 함
+// viedoRouter.route("/upload").get(getUpload).post(postUpload);
 // id로 인식하기 때문에 가장 최상단에 위치해야함
 // viedoRouter.get("/upload", upload);
 // id는 숫자만 받을 수 있도록 정규식 적용. 따라서, upload 라우터가 id에 영향을 받지 않음
 // id 객체를 params로 받을 수 있도록 key를 id로 설정
-viedoRouter.get("/:id(\\d+)", watch);
+// /[0-9a-f]{24}/ REGEX <- mongodb id가 16진수 24바이트 (Can be a 24 byte hex string)
+viedoRouter.get("/:id([0-9a-f]{24})", watch);
 // viedoRouter.get("/:id(\\d+)/edit", getEdit);
 // viedoRouter.post("/:id(\\d+)/edit", postEdit);
-viedoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+viedoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 viedoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default viedoRouter;
